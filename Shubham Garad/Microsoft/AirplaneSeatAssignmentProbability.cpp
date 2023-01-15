5ae4acd63f4cb1c5c1f9668c2de6a6cdfc065de2
Shubham <garadshubham343@gmail.com>
2023-01-15 22:26:33 +0530

    Day5 ques1

---
+++ Shubham Garad/Microsoft/AirplaneSeatAssignmentProbability.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    double nthPersonGetsNthSeat(int n) {
+        
+        if (n==1)
+        {
+            return 1.0;
+        }
+
+        return (double)1/(double)2;
+    }
+};
