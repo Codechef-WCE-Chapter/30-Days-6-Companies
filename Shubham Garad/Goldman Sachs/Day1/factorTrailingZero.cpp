2624fde9043826f0162d4d15913c73615e44d0ac
Shubham <garadshubham343@gmail.com>
2023-01-20 11:13:13 +0530

    Day6 ques 3

---
+++ Shubham Garad/Goldman Sachs/Day1/factorTrailingZero.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+
+     int count =0 ;
+     while(n>0)
+     {
+         n = n/5;
+         count += n;
+     }   
+
+     return count;
+    }
+};
