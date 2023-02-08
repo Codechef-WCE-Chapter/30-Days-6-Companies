6953d3282ca437e3d4996df0d3d9506d041ee440
vishal <vislearn1983@gmail.com>
2023-02-09 00:30:10 +0530

    Day28 2Q Done

---
+++ Vishal-Chauhan/Amazon/Day27/pract2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        cout << (n + 1) / 10 << endl;
+    }
+}
